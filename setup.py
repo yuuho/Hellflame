cd210b3ff9af2ac1638d33758f7ac9ee691e254c
yuuho <y.horiuchi@suou.waseda.jp>
2019-04-06 20:14:04 +0900

    バージョン番号の修正

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="hellfire",
-    version="0.0.1",
+    version="0.0.2",
     packages=['hellfire','hellfire.services'],
     author="yuuho",
     author_email="y.horiuchi@suou.waseda.jp",
