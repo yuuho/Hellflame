283e75710e15644612033115520b830e558ed112
yuuho <y.horiuchi@suou.waseda.jp>
2019-04-06 17:26:37 +0900

    save

--- hellfire/__init__.py
+++
@@ -1,1 +0,0 @@
-from .hellfire import *
--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import setup
 setup(
     name="hellfire",
     version="0.0.1",
+    package_dir={'': 'hellfire'},
+    packages=[''],
     author="yuuho",
     author_email="y.horiuchi@suou.waseda.jp",
     url="https://github.com/yuuho/Hellfire",
     description="Object-Oriented research tool for deep learning",
     entry_points={
         "console_scripts": [
-            "hellfire = hellfire.hellfire:main"
+            "hellfire = hellfire:main"
         ]
     }
 )
