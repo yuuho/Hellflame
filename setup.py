ad041b3652cf2b9a69fa5826b1c6c4b9fb5821b6
yuuho <y.horiuchi@suou.waseda.jp>
2019-04-06 17:27:52 +0900

    save

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="hellfire",
     version="0.0.1",
-    package_dir={'': 'hellfire'},
+    package_dir={'': 'src'},
     packages=[''],
     author="yuuho",
     author_email="y.horiuchi@suou.waseda.jp",
