3317f35466c244a522b1c60976870b1ad8d37064
yuuho <y.horiuchi@suou.waseda.jp>
2019-04-06 20:11:46 +0900

    パスの読み込みを修正

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
 clean:
 	find ./ -name '__pycache__' | xargs rm -rf
 	find ./ -name '*.pyc' | xargs rm -f
+
+uninstall:
+	pip uninstall -y hellfire
+
+install:
+	pip install git+https://github.com/yuuho/Hellfire
--- hellfire/services/TrainService.py
+++ hellfire/services/TrainService.py
@@ -1,4 +1,10 @@
 
+import os
+from argpase import Namespace
+from pathlib import Path
+
+import yaml
+
 from hellfire.services.Service import Service
 
 
@@ -14,9 +20,11 @@ class TrainService(Service):
         # 必須項目
         parser.add_argument('--config','-c',type=str,
                             default=None,
+                            required=True,
                             dest='config', help='config file path')
         parser.add_argument('--name','-n',type=str,
                             default=None,
+                            required=True,
                             dest='name', help='experiment name')
         # 環境変数からも読み込める
         parser.add_argument('--programs','-p', type=str,
@@ -32,15 +40,64 @@ class TrainService(Service):
                             default=None,
                             dest='tmp', help='一時ファイル置き場')
 
+
     # エントリーポイント
     def handler_function(self,args):
+        # 設定ファイルの存在確認
+        paths = self.get_paths(args) # paths : Namespace, all attr is Path
 
-        # argsのvalidation
+        print(paths)
 
         # 読み込み
-
         print('here do train')
 
-    def validate_args(self,args):
-        """コマンドライン引数が正しいか確認"""
-        return args
+
+    def get_paths(self,args):
+        """パスの設定"""
+        #paths = Namespace()
+
+        path_names = ['prog','data','exp','tmp']
+        env_names = [ 'ML'+name.upper() for name in path_names]
+        paths = {k:{'path':None,'src':None} in path_names}
+
+        # プログラムだけ初期設定はカレントディレクトリ
+        paths['prog'] = {'paht':Path(os.getcwd()),'src':'current path'}
+
+        # 環境変数の読み込み
+        for pname, ename in zip(path_names,env_names):
+            if os.environ.get(ename) is not None:
+                paths[pname] = {'path':Path(os.environ.get(ename)),'src':'environment variable'}
+
+        # 設定ファイルの存在確認
+        paths.config = Path(args.config)
+        if not paths.config.exists():
+            raise Exception('there is not such a config file : '+paths.config)
+        else:
+            with open(str(paths.config),'r') as f:
+                config = yaml.load(f)
+
+        # 設定ファイルからパスの読み込み
+        if 'path' in config['environ']:
+            for pname, ename in zip(path_names,env_names):
+                if ename in config['environ']['path']:
+                    paths[pname] = {'path':Path(config['environ']['path'][ename]),'src':'config file'}
+
+        # コマンドライン引数からの読み込み
+        for pname in path_names:
+            if vars(args)[pname] is not None:
+                paths[pname] = {'path':Path(vars(args)[pname]),'src':'commandline args'}
+
+        # 存在確認
+        for pname, ename in zip(path_names,env_names):
+            if paths[pname]['path'] is None:
+                raise Exception('set path : '+pname+' ($'+ename+')')
+            if not paths[pname]['path'].exists():
+                raise Exception('the path is not exist : '+paths[pname]['path']+' read from '+paths[pname]['src']+\
+                                'set accurate path : '+pname+' ($'+ename+')')
+
+        # Namespaceに格納
+        result_paths = Namespace()
+        result_paths.__dict__.update({k:v['path'] for k,v in paths.items()})
+
+        return result_paths
+
