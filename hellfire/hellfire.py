045f0c7852f885ed415c9b4933ea265bd82e2a5c
yuuho <y.horiuchi@suou.waseda.jp>
2019-09-09 14:45:28 +0900

    出力を分割されないようにひとつのprint関数に収めた

--- hellfire/hellfire.py
+++ hellfire/hellfire.py
@@ -2,7 +2,7 @@ import argparse
 from importlib import import_module
 
 
-__version__ = '0.2.0'
+__version__ = '0.2.2'
 
 
 class Hellfire:
@@ -10,10 +10,10 @@ class Hellfire:
     service_names = ['train','doctor']
 
     def __init__(self):
-        print('===================================================================')
-        print('| Hellfire Start  %s ( ver. %s )                            |'%(' '*(10-len(__version__)),__version__))
-        print('===================================================================')
-        
+        print('===================================================================\n' \
+             '| Hellfire Start  %s ( ver. %s )                            |\n' \
+             '==================================================================='%(' '*(10-len(__version__)),__version__))
+
         # クラス変数の読み込み
         self.service_names = Hellfire.service_names
 
@@ -29,9 +29,9 @@ class Hellfire:
         # 実行 or エラーヘルプ
         result = args.handler(args) if hasattr(args,'handler') else self.parser.print_help()
 
-        print('===================================================================')
-        print('| Hellfire End                                                    |')
-        print('===================================================================')
+        print('===================================================================\n' \
+              '| Hellfire End                                                    |\n' \
+              '===================================================================')
 
 
     # 存在するすべてのサービスへの案内を準備
