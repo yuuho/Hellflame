a277358fc5d7ff3b70af3436d53d319a5970eda4
yuuho <y.horiuchi@suou.waseda.jp>
2020-12-28 00:07:54 +0900

    ユースケースをメモ書き

--- hellflame/snippets/relative_object_loader.py
+++ hellflame/snippets/relative_object_loader.py
@@ -10,6 +10,17 @@ def load_object(path, key):
 
 
 '''
+from hellflame.snippets.relative_object_loader import load_object
+
+# 相対パスでも動くし
+
 Hoge = load_object('../../foo/bar/baz.py', 'Hoge')
 Fuga = load_object('../../foo/bar/baz.py', 'Fuga')
+
+
+# 絶対パスでも正しく動く
+
+Hoge = load_object('/home/hoge/foo/bar/baz.py', 'Hoge')
+Fuga = load_object('/home/hoge/foo/bar/baz.py', 'Fuga')
+
 '''
